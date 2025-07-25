87876acc966a979183bde9f0fc79fe75d6ca95d2
Vitor Severo <vitor.severo@sou.unifeob.edu.br>
2025-07-25 00:26:25 -0300

    Adicionando novas informações das aulas

--- cerebrogo.go
+++ cerebrogo.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-
+    //Bloco solto
+	// var x string = "Hello"
 
 func main(){
 	// fmt.Println("Hello, World")
@@ -24,7 +25,20 @@ func main(){
 	// fmt.Println("Ola", nome, "vi que tem", idade,"anos")
 	// fmt.Println("Nosso teste esta no inicio, na versao", versao)
     
-	var x = "Hello World"
-	 fmt.Println(x)
+	// var x = "Hello World"
+	//  fmt.Println(x)
+
+	// var x string 
+	// x = "Hello World"
+	//  fmt.Println(x)
+
+	 //Aula Bloco e escopo 
+	 // 	fmt.Println(x)
+
+	 //Aula Arrays, Fariaas e Mapas
+	  var x [5]int  // Uma array sempre e declarada entre [e colchetes e o numero de elementos que ela tera, nesse caso 5] 
+	  x[3] = 50 // E para definir o valor de um elemento dentro da array usamos o nome da variavel com uma nova array com o numero da que vai definir o valor e posição do elemento dentro da array, nesse caso o 3 que e o quarto elemento, pois a contagem começa do zero. em 
+	  fmt.Println(x)
+	 
 }
 
