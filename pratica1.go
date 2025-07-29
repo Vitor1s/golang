5aa59b7148ca04322630cb98adb5a9175130aa21
Vitor Severo <vitor.severo@sou.unifeob.edu.br>
2025-07-29 18:48:37 -0300

    estudos Go

---
+++ pratica1.go
@@ -0,0 +1,17 @@
+// package main 
+
+// import "fmt"
+
+// const aguaquente = 212.0
+
+// func main(){
+
+// 	var tempF = aguaquente
+// 	var tempC = (tempF - 32) * 5/9
+// 	fmt.Println("A temperatura da agua quente em embulição Fahrenheit e: ", tempF)
+// 	fmt.Println("A temperatura da agua quente em embulição Celsius e: ", tempC)
+// }
+
+// TODO. O objetivo dessa prática foi aprender a trabalhar com variaveis e constantes, e entender como o Go reconhece os tipos de acordo com o que foi atribuido. 
+
+//TODO. Observações a serem feitas, nao defini os tipos pois o Go reconhece automaticamente os tipos de acordo com o que foia tribuido.
---
+++ pratica3.go
@@ -0,0 +1,17 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	pratica3()
+
+// }
+
+// TODO. No pratica3 vou usar o Printfn(Print Format) para ver qual a diferença e utilidade dele comparado ao Println(Print Line) que printa somenta a linha que estamos referenciando. Trocando os Printsln vem a questão de como vamos usar o Printfn. Mostrarei no codigo abaixo. Detalhe que agora estando na mesma linha temos que dar um jeito de a resposta aparecer a resposta de C e F, e usaremos um caractere %g que é usado em Go, e exxste outros que usaremos.(OBS: \n e um simbulo de quebra de linha pois o Printfn nao aceita quebra de linha.)
+
+// func pratica3() {
+// 	aguaquente := 212.0
+// 	F := aguaquente
+// 	C := (F - 32) * 5 / 9
+// 	fmt.Printf("A temperatura em Celsius é = %g A temperatuira em Fahrenheit é = %g.\n", C, F)
+// }
